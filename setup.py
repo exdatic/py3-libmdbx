ab091cbccc7ab3deb7ba483b7852b7b3af24599e
Noel Kuntze <noel.kuntze@thermi.consulting>
2021-07-10 01:00:07 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 
-# TODO: like https://github.com/jnwatson/py-lmdb/blob/master/setup.py
+import distutils.command.build
+import setuptools
 
-from distutils.core import setup
+import os
+import subprocess
 
-setup(name='libmdbx',
-      version='0.10.1',
+version="0.10.1"
+try:
+      with open("VERSION", "r") as fd:
+            version=fd.read().decode("utf-8")
+            version=version.strip()
+except:
+      pass
+
+
+class poke_libpath(setuptools.Command):
+    #user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+      # poke lib location into code
+      lib_location=os.environ.get("MDBX_LIB_LOCATION", "/usr/lib/libmdbx.so")
+      proc=subprocess.run(["sed", "s%@MDBX_SOLIB_LOCATION@%{}%".format(lib_location), "libmdbx/mdbx.py.in"],
+            capture_output=True)
+      with open("libmdbx/mdbx.py", "w") as fd:
+            fd.write(proc.stdout.decode("utf-8"))
+class build(distutils.command.build.build):
+    _sub_command = (
+        'poke_libpath',
+        None,
+    )
+    _sub_commands = distutils.command.build.build.sub_commands
+    sub_commands = [_sub_command] + _sub_commands
+
+
+setuptools.setup(name='libmdbx',
+      version=version,
       description='Bindings for mdbx',
       author='Noel Kuntze',
       author_email='noel.kuntze@thermi.consulting',
@@ -13,4 +46,9 @@ setup(name='libmdbx',
       license='SPDX-License-Identifier: OLDAP-2.8',
       keywords=['key-value', 'nosql', 'database', 'mdbx', 'libmdbx', 'lmdb'],
       packages=['libmdbx'],
-     )
+      install_requires=['ctypes'],
+      cmdclass={
+            'poke_libpath': poke_libpath,
+            'build': build,
+      },
+)
